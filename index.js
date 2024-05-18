a1ebabd98576fffc2eb17f54e5ae5cb049170f20
makalaosiggins <wrxmakala@icloud.com>
2024-05-17 20:34:43 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+console.log(multiply);
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+console.log(mod);
+
+const numbers = [10, 15, 20, 5];
+const max = Math.max(...numbers);
+console.log(max);
